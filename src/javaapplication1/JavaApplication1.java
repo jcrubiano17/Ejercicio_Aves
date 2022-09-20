3930a72f3fc6cdc6bf32263aeb30aa9afc17f957
Jose Cruz <jdcruz70@ucatolica.edu.co>
2022-09-20 13:03:25 -0500

    1.0.1 - primera prueba

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author sala8
+ */
+public class JavaApplication1 {
+    
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
