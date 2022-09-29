40201101667b53b6cd0b6afd31fb336c9fee359f
Juan Camilo <114019169+jcrubiano17@users.noreply.github.com>
2022-09-28 23:46:50 -0500

    Add files via upload

---
+++ Ejercicio_Ave.java
@@ -0,0 +1,16 @@
+package ejercicio_ave;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Ejercicio_Ave {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
